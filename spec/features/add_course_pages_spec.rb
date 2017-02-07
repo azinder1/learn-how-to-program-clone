2ef92716ec01b8dce41d2cbbb63ec43d1aae2464
Adam Zinder and Tracie Weitzman <adam+tracie@>
2017-02-07 14:04:22 -0800

    add styling to courses page, it is awesome

--- spec/features/add_course_pages_spec.rb
+++ spec/features/add_course_pages_spec.rb
@@ -6,7 +6,7 @@ describe "the add a new course process" do
     click_on "Add Course"
     fill_in 'Name', :with => "Ruby"
     click_on "Create Course"
-    expect(page).to have_content "Learn"
+    expect(page).to have_content "Courses"
   end
   it "gives error when course name isn't entered" do
     visit new_course_path
